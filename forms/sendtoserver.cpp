30f42ac099164d05070e483caa49bb60a3c31bc3
test <6141242@gmail.com>
2015-08-19 13:40:31 +0300

    new class

---
+++ forms/sendtoserver.cpp
@@ -0,0 +1,12 @@
+#include "sendtoserver.h"
+
+sendToServer::sendToServer()
+{
+
+}
+
+sendToServer::~sendToServer()
+{
+
+}
+
---
+++ forms/sendtoserver.h
@@ -0,0 +1,12 @@
+#ifndef SENDTOSERVER_H
+#define SENDTOSERVER_H
+
+
+class sendToServer
+{
+public:
+    sendToServer();
+    ~sendToServer();
+};
+
+#endif // SENDTOSERVER_H
